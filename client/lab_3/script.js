e51d12a99ce9f5ac93aecca62257e69fc626a2c7
salonigupta9 <sgupta09@terpmail.umd.edu>
2023-02-19 15:58:15 -0500

    final

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -23,7 +23,8 @@ function updateSlidePosition() {
     slide.classList.remove('visible');
     slide.classList.add('hidden');
 
-  })
+  });
+  console.log(slidePosition);
   slides[slidePosition].classList.add('visible');
 
 
@@ -43,6 +44,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if(slidePosition === totalSlides - 1){
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -53,6 +59,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+  if(slidePosition === 0){
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition = slidePosition - 1;
+  }
   updateSlidePosition();
 }
 
